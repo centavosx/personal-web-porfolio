87bb05f9c0fda334150f300c0b6297c1efb9fc67
Vincent Llanto <vincentllanto098123@gmail.com>
2025-01-31 19:42:56 +0800

    Fix lag when scrolling

--- src/hooks/useDetectActiveHash.ts
+++ src/hooks/useDetectActiveHash.ts
@@ -1,25 +1,6 @@
 import { useEffect, useState } from "react";
 import { useThrottle } from "./useThrottle";
 
-const historyReplaceState = (
-  ...args: Parameters<typeof history.replaceState>
-) => {
-  // It returns an error when replacing a state when manually inputting it in a location bar
-  // ERROR: Cannot read properties of null (reading '__PRIVATE_NEXTJS_INTERNALS_TREE')
-  try {
-    const origin = window.location.origin;
-    const href = window.location.href.replace(origin, "");
-    let url = args[2];
-    if (typeof url === "string") {
-      url = url.replace(origin, "");
-    }
-
-    if (href === url) return;
-
-    history.replaceState(...args);
-  } catch {}
-};
-
 const checkIdElementIsVisible = (id: string) => {
   const anchorId = id.replace("#", "");
   const element = document.getElementById(anchorId);
@@ -53,15 +34,12 @@ export const useDetectActiveHash = ({
   noDeadHashZonePercentage = 0.2,
 }: DetectActiveHashOptions) => {
   const [currentHash, setCurrentHash] = useState<string | null>(null);
-  const debouncedHistoryReplaceState = useThrottle(historyReplaceState, 60);
 
   const handleScrollEventListener = useThrottle(() => {
     // syncs the current hash with the current visible section
     const updateHashState = () => {
       for (const targetHash of linkHashes) {
         if (checkIdElementIsVisible(targetHash)) {
-          const [url] = window.location.toString().split("#");
-          debouncedHistoryReplaceState(null, "", url + targetHash);
           setCurrentHash(targetHash);
           return;
         }
@@ -71,16 +49,12 @@ export const useDetectActiveHash = ({
     const NO_HASH_DEADZONE = window.innerHeight * noDeadHashZonePercentage;
 
     if (window.scrollY < NO_HASH_DEADZONE) {
-      if (!location.hash) {
-        return;
-      }
       setCurrentHash(null);
-      debouncedHistoryReplaceState(null, "", pageUrl);
       return;
     } else {
       updateHashState();
     }
-  });
+  }, 30);
 
   useEffect(() => {
     setCurrentHash(window.location.hash);
